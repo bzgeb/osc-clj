d999a6159035aa612f5d737db444ff22a88a79c2
Bronson Zgeb <bronson.zgeb@gmail.com>
2012-08-22 09:10:43 -0400

    Removed JMDNS JMDNS is only necessary for zero-conf functions, which have been removed

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject overtone/osc-clj "0.7.1"
+(defproject overtone/osc-clj "0.7.1-bzgeb"
   :description "An Open Sound Control library for Clojure."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.technomancy/jmdns "3.2.1"]
                  [commons-net "3.0.1"]
                  [overtone/at-at "0.2.1"]])
